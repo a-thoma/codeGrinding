60d4432a75527369dd2c10a84ecc7d034c427dd2
Fudgyone <thomaa1@unlv.nevada.edu>
2017-07-19 10:08:02 -0700

    Started evenFib

---
+++ evenFib/main.cpp
@@ -0,0 +1,13 @@
+/**
+* projectEuler problem 2: Even Fibonacci numbers below four million
+*
+* Find the sum of all the even numbers in the Fibonacci sequence
+* below four million.
+*/
+
+#include <iostream>
+
+int main() {
+	
+	return EXIT_SUCCESS;
+}
