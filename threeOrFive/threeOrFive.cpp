36fa790e2e3bebdb4eee0fd8205c4cff486591de
Fudgyone <thomaa1@unlv.nevada.edu>
2017-07-19 10:01:50 -0700

    Finished

--- threeOrFive/threeOrFive.cpp
+++ threeOrFive/threeOrFive.cpp
@@ -1,4 +1,4 @@
-/**
+/********************************************************
 * projectEuler Problem 1: Multiples of 3 and 5
 *
 * Find the sum of all the multiples of 3 or 5 below 1000.
@@ -8,7 +8,7 @@
 
 int main() {
 	
-/**
+/******************************************************************************
 * Note: Fastest way to do this (fastest in terms of writing, not execution)
 * is to just sum anything below 1000 that, when mod 3 or mod 5, evaluates to 0.
 *
@@ -22,9 +22,12 @@ int main() {
 	int divFive    = 5  * ((199 * 200) / 2);
 	int divFifteen = 15 * ((66  *  67) / 2);
 
+	// Sum using PIE, i.e. S = A + B - C.
 	int sum = divThree + divFive - divFifteen;
 
+	// Print the sum of all multiples of 3 or 5 below 1000.
 	std::cout << "Sum of all mutliples of 3 or 5 below 1000: " << sum << std::endl;
 
+	// We're done.
 	return EXIT_SUCCESS;
 }
