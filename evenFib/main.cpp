78b6be3efadbbd4c5f922123c922cec8ed510101
Fudgyone <thomaa1@unlv.nevada.edu>
2017-07-19 10:24:33 -0700

    Finished

--- evenFib/main.cpp
+++ evenFib/main.cpp
@@ -1,5 +1,5 @@
-/**
-* projectEuler problem 2: Even Fibonacci numbers below four million
+/*******************************************************************
+* projectEuler problem 2: Even Fibonacci numbers below four million.
 *
 * Find the sum of all the even numbers in the Fibonacci sequence
 * below four million.
@@ -8,6 +8,22 @@
 #include <iostream>
 
 int main() {
-	
+
+	int sum  = 0; // Running sum
+	int fOne = 0; // Old fib number
+	int fTwo = 1; // New/next fib number
+
+	while (fTwo < 4000000) {     // Anything below four million
+		if (fTwo%2 == 0) {       // If it's even...
+			sum += fTwo;         // Add to our total
+		}
+			fOne = fTwo;         // New value becomes old value
+			fTwo = fTwo + fOne;  // Update new value
+	}
+
+	// Print the sum
+	std::cout << "Sum of all even numbers below four million: " << sum << std::endl;
+
+	// We're done
 	return EXIT_SUCCESS;
 }
